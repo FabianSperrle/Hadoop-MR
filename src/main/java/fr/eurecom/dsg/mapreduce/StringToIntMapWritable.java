1c9d47cbbf2e91fa4e2eeeb534e53698b5433a82
Fabian Sperrle <fabian.sperrle@uni.kn>
2016-10-28 11:39:17 +0200

    Add striping... first try

--- src/main/java/fr/eurecom/dsg/mapreduce/StringToIntMapWritable.java
+++ src/main/java/fr/eurecom/dsg/mapreduce/StringToIntMapWritable.java
@@ -1,31 +1,65 @@
 package fr.eurecom.dsg.mapreduce;
+
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
+
 /*
  * Very simple (and scholastic) implementation of a Writable associative array for String to Int 
  *
  **/
 public class StringToIntMapWritable implements Writable {
-  
-  // TODO: add an internal field that is the real associative array
 
-  @Override
-  public void readFields(DataInput in) throws IOException {
-    
-    // TODO: implement deserialization
-    
-    // Warning: for efficiency reasons, Hadoop attempts to re-use old instances of
-    // StringToIntMapWritable when reading new records. Remember to initialize your variables 
-    // inside this function, in order to get rid of old data.
+    private Map<String, Integer> map;
+    private Text TMP_TEXT;
 
-  }
+    public StringToIntMapWritable() {
+        this.map = new HashMap<>();
+        this.TMP_TEXT = new Text();
+    }
 
-  @Override
-  public void write(DataOutput out) throws IOException {
+    public StringToIntMapWritable(Map<String, Integer> map) {
+        this.map = map;
+        this.TMP_TEXT = new Text();
+    }
 
-    // TODO: implement serialization
-  }
+    public void set(Map<String, Integer> map) {
+        this.map = map;
+    }
+
+    public Map<String, Integer> getMap() {
+        return this.map;
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        this.map = new HashMap<>();
+
+        int num_words = in.readInt();
+        for (int i = 0; i < num_words; i++) {
+            TMP_TEXT.readFields(in);
+            int count = in.readInt();
+
+            map.put(TMP_TEXT.toString(), count);
+        }
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        // Write map size
+
+        int count = 0;
+        for (Map.Entry<String, Integer> entry : this.map.entrySet()) {
+            TMP_TEXT.set(entry.getKey());
+            TMP_TEXT.write(out);
+
+            count = entry.getValue();
+            out.writeInt(count);
+        }
+    }
 }
--- src/main/java/fr/eurecom/dsg/mapreduce/Stripes.java
+++ src/main/java/fr/eurecom/dsg/mapreduce/Stripes.java
@@ -1,83 +1,153 @@
 package fr.eurecom.dsg.mapreduce;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 
 public class Stripes extends Configured implements Tool {
 
-  private int numReducers;
-  private Path inputPath;
-  private Path outputDir;
+    private int numReducers;
+    private Path inputPath;
+    private Path outputDir;
 
-  @Override
-  public int run(String[] args) throws Exception {
-    
-    Configuration conf = this.getConf();
-    Job job = null;  // TODO: define new job instead of null using conf e setting a name
-    
-    // TODO: set job input format
-    // TODO: set map class and the map output key and value classes
-    // TODO: set reduce class and the reduce output key and value classes
-    // TODO: set job output format
-    // TODO: add the input file as job input (from HDFS) to the variable inputFile
-    // TODO: set the output path for the job results (to HDFS) to the variable outputPath
-    // TODO: set the number of reducers using variable numberReducers
-    // TODO: set the jar class
+    @Override
+    public int run(String[] args) throws Exception {
 
-    return job.waitForCompletion(true) ? 0 : 1;
-  }
+        Configuration conf = this.getConf();
+        Job job = new Job(conf, "EmilFabian Stripes");
 
-  public Stripes (String[] args) {
-    if (args.length != 3) {
-      System.out.println("Usage: Stripes <num_reducers> <input_path> <output_path>");
-      System.exit(0);
+        job.setInputFormatClass(TextInputFormat.class);
+
+        job.setMapperClass(StripesMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(StringToIntMapWritable.class);
+
+        job.setReducerClass(StripesReducer.class);
+        job.setOutputKeyClass(TextPair.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, this.inputPath);
+
+        FileOutputFormat.setOutputPath(job, this.outputDir);
+
+        job.setNumReduceTasks(this.numReducers);
+
+        job.setJarByClass(Stripes.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
     }
-    this.numReducers = Integer.parseInt(args[0]);
-    this.inputPath = new Path(args[1]);
-    this.outputDir = new Path(args[2]);
-  }
-  
-  public static void main(String[] args) throws Exception {
-    int res = ToolRunner.run(new Configuration(), new Stripes(args), args);
-    System.exit(res);
-  }
+
+    public Stripes(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: Stripes <num_reducers> <input_path> <output_path>");
+            System.exit(0);
+        }
+        this.numReducers = Integer.parseInt(args[0]);
+        this.inputPath = new Path(args[1]);
+        this.outputDir = new Path(args[2]);
+    }
+
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new Stripes(args), args);
+        System.exit(res);
+    }
 }
 
-class StripesMapper
-extends Mapper<Object,   // TODO: change Object to input key type
-               Object,   // TODO: change Object to input value type
-               Object,   // TODO: change Object to output key type
-               Object> { // TODO: change Object to output value type
+class StripesMapper extends Mapper<LongWritable, Text, Text, StringToIntMapWritable> {
 
-  @Override
-  public void map(Object key, // TODO: change Object to input key type
-                  Object value, // TODO: change Object to input value type
-                  Context context)
-  throws java.io.IOException, InterruptedException {
+    private String[] iteratorToTokenArray(StringTokenizer tok) {
+        String[] tokens = new String[tok.countTokens()];
+        int i = 0;
+        while (tok.hasMoreTokens()) {
+            tokens[i++] = tok.nextToken();
+        }
+        return tokens;
+    }
 
-    // TODO: implement map method
-  }
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws java.io.IOException, InterruptedException {
+        Map<String, Map<String, Integer>> map = new HashMap<>();
+
+        StringTokenizer tok = new StringTokenizer(value.toString());
+        String[] tokens = iteratorToTokenArray(tok);
+
+        for (String ref : tokens) {
+            Map<String, Integer> innerMap;
+            if (map.containsKey(ref)) {
+                innerMap = map.get(ref);
+            } else {
+                innerMap = new HashMap<>();
+            }
+            for (String neighbour : tokens) {
+                if (ref.equals(neighbour)) {
+                    continue;
+                }
+
+                if (innerMap.containsKey(neighbour)) {
+                    innerMap.put(neighbour, innerMap.get(neighbour) + 1);
+                } else {
+                    innerMap.put(neighbour, 1);
+                }
+            }
+            map.put(ref, innerMap);
+        }
+
+        Text TMP_TEXT = new Text();
+        StringToIntMapWritable TMP_SIM = new StringToIntMapWritable();
+        for (Map.Entry<String, Map<String, Integer>> entry : map.entrySet()) {
+            TMP_TEXT.set(entry.getKey());
+            TMP_SIM.set(entry.getValue());
+
+            context.write(TMP_TEXT, TMP_SIM);
+        }
+    }
 }
 
-class StripesReducer
-extends Reducer<Object,   // TODO: change Object to input key type
-                Object,   // TODO: change Object to input value type
-                Object,   // TODO: change Object to output key type
-                Object> { // TODO: change Object to output value type
-  @Override
-  public void reduce(Object key, // TODO: change Object to input key type
-                     Iterable<Object> values, // TODO: change Object to input value type 
-                     Context context) throws IOException, InterruptedException {
+class StripesReducer extends Reducer<Text, StringToIntMapWritable, TextPair, IntWritable> {
 
-    // TODO: implement the reduce method
-  }
+    @Override
+    public void reduce(Text key, Iterable<StringToIntMapWritable> values, Context context) throws IOException, InterruptedException {
+        HashMap<TextPair, Integer> map = new HashMap<>();
+
+        for (StringToIntMapWritable sim_map : values) {
+            for (Map.Entry<String, Integer> entry : sim_map.getMap().entrySet()) {
+                Text text = new Text(entry.getKey());
+                TextPair tp = new TextPair(key, text);
+                int count = entry.getValue();
+
+                if (map.containsKey(tp)) {
+                    map.put(tp, map.get(tp) + count);
+                } else {
+                    map.put(tp, count);
+                }
+            }
+        }
+
+        IntWritable TMP_INT = new IntWritable();
+        for (Map.Entry<TextPair, Integer> entry : map.entrySet()) {
+            TMP_INT.set(entry.getValue());
+            context.write(entry.getKey(), TMP_INT);
+        }
+    }
 }
