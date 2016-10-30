f8ec6b724cda9bf08ed340e7ee9b2bb923f40aae
Fabian Sperrle <fabian.sperrle@uni.kn>
2016-10-30 14:56:21 +0100

    First version of order inversion

--- src/main/java/fr/eurecom/dsg/mapreduce/OrderInversion.java
+++ src/main/java/fr/eurecom/dsg/mapreduce/OrderInversion.java
@@ -17,84 +17,154 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 
 public class OrderInversion extends Configured implements Tool {
 
-  private final static String ASTERISK = "\0";
+    private final static String ASTERISK = "\0";
 
-  public static class PartitionerTextPair extends
-  Partitioner<TextPair, IntWritable> {
+    public static class PartitionerTextPair extends Partitioner<TextPair, IntWritable> {
+
+        @Override
+        public int getPartition(TextPair key, IntWritable value, int numPartitions) {
+            return toUnsigned(key.getFirst().hashCode()) % numPartitions;
+        }
+
+        /**
+         * toUnsigned(10) = 10
+         * toUnsigned(-1) = 2147483647
+         *
+         * @param val Value to convert
+         * @return the unsigned number with the same bits of val
+         */
+        public static int toUnsigned(int val) {
+            return val & Integer.MAX_VALUE;
+        }
+    }
+
+    public static class PairMapper extends Mapper<LongWritable, Text, TextPair, IntWritable> {
+        private final Text REF = new Text();
+        private final Text NEIGHBOR = new Text();
+        private final TextPair PAIR = new TextPair();
+        private final IntWritable ONE = new IntWritable(1);
+
+        private String[] iteratorToTokenArray(StringTokenizer tok) {
+            String[] tokens = new String[tok.countTokens()];
+            int i = 0;
+            while (tok.hasMoreTokens()) {
+                tokens[i++] = tok.nextToken();
+            }
+            return tokens;
+        }
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws java.io.IOException, InterruptedException {
+            StringTokenizer tok = new StringTokenizer(value.toString());
+            String[] tokens = iteratorToTokenArray(tok);
+
+            for (String ref : tokens) {
+                this.REF.set(ref);
+                for (String neighbour : tokens) {
+                    if (ref.equals(neighbour)) continue;
+
+                    this.NEIGHBOR.set(neighbour);
+                    this.PAIR.set(REF, NEIGHBOR);
+                    context.write(this.PAIR, ONE);
+
+                    this.NEIGHBOR.set(ASTERISK);
+                    this.PAIR.set(REF, NEIGHBOR);
+                    context.write(this.PAIR, ONE);
+                }
+            }
+        }
+    }
+
+    public static class PairCombiner extends Reducer<TextPair, IntWritable, TextPair, IntWritable> {
+        private final IntWritable COUNT = new IntWritable();
+
+        @Override
+        protected void reduce(TextPair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable i : values) {
+                sum += i.get();
+            }
+
+            this.COUNT.set(sum);
+            context.write(key, this.COUNT);
+        }
+    }
+
+    public static class PairReducer extends Reducer<TextPair, IntWritable, TextPair, DoubleWritable> {
+        private static Map<String, Integer> marginalMap;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            super.setup(context);
+
+            marginalMap = new HashMap<>();
+        }
+
+        @Override
+        protected void reduce(TextPair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            String word = key.getFirst().toString();
+            if (key.getSecond().toString().equals(ASTERISK)) {
+                int sum = 0;
+                for (IntWritable i : values) {
+                    sum += i.get();
+                }
+
+                marginalMap.put(word, sum);
+            } else {
+                double sum = 0.0;
+                for (IntWritable i : values) {
+                    sum += i.get();
+                }
+
+                int total = marginalMap.get(word);
+                context.write(key, new DoubleWritable(sum / total));
+            }
+        }
+    }
+
+    private int numReducers;
+    private Path inputPath;
+    private Path outputDir;
+
     @Override
-    public int getPartition(TextPair key, IntWritable value,
-        int numPartitions) {
-      // TODO: implement getPartition such that pairs with the same first element
-      //       will go to the same reducer. You can use toUnsighed as utility.
-      return 0;
+    public int run(String[] args) throws Exception {
+        Configuration conf = this.getConf();
+
+        Job job = null;  // TODO: define new job instead of null using conf e setting a name
+
+        // TODO: set job input format
+        // TODO: set map class and the map output key and value classes
+        // TODO: set reduce class and the reduce output key and value classes
+        // TODO: set job output format
+        // TODO: add the input file as job input (from HDFS) to the variable inputFile
+        // TODO: set the output path for the job results (to HDFS) to the variable outputPath
+        // TODO: set the number of reducers using variable numberReducers
+        // TODO: set the jar class
+
+        return job.waitForCompletion(true) ? 0 : 1;
     }
-    
-    /**
-     * toUnsigned(10) = 10
-     * toUnsigned(-1) = 2147483647
-     * 
-     * @param val Value to convert
-     * @return the unsigned number with the same bits of val 
-     * */
-    public static int toUnsigned(int val) {
-      return val & Integer.MAX_VALUE;
+
+    OrderInversion(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: OrderInversion <num_reducers> <input_file> <output_dir>");
+            System.exit(0);
+        }
+        this.numReducers = Integer.parseInt(args[0]);
+        this.inputPath = new Path(args[1]);
+        this.outputDir = new Path(args[2]);
     }
-  }
 
-  public static class PairMapper extends
-  Mapper<LongWritable, Text, TextPair, IntWritable> {
-
-    @Override
-    public void map(LongWritable key, Text value, Context context)
-    throws java.io.IOException, InterruptedException {
-
-      // TODO: implement the map method
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new OrderInversion(args), args);
+        System.exit(res);
     }
-  }
-
-  public static class PairReducer extends
-  Reducer<TextPair, IntWritable, TextPair, DoubleWritable> {
-
-    // TODO: implement the reduce method
-  }
-
-  private int numReducers;
-  private Path inputPath;
-  private Path outputDir;
-
-  @Override
-  public int run(String[] args) throws Exception {
-    Configuration conf = this.getConf();
-
-    Job job = null;  // TODO: define new job instead of null using conf e setting a name
-    
-    // TODO: set job input format
-    // TODO: set map class and the map output key and value classes
-    // TODO: set reduce class and the reduce output key and value classes
-    // TODO: set job output format
-    // TODO: add the input file as job input (from HDFS) to the variable inputFile
-    // TODO: set the output path for the job results (to HDFS) to the variable outputPath
-    // TODO: set the number of reducers using variable numberReducers
-    // TODO: set the jar class
-    
-    return job.waitForCompletion(true) ? 0 : 1;
-  }
-
-  OrderInversion(String[] args) {
-    if (args.length != 3) {
-      System.out.println("Usage: OrderInversion <num_reducers> <input_file> <output_dir>");
-      System.exit(0);
-    }
-    this.numReducers = Integer.parseInt(args[0]);
-    this.inputPath = new Path(args[1]);
-    this.outputDir = new Path(args[2]);
-  }
-  
-  public static void main(String[] args) throws Exception {
-    int res = ToolRunner.run(new Configuration(), new OrderInversion(args), args);
-    System.exit(res);
-  }
 }
